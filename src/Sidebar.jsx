615afb9624c9050c4617e75427faf04f2f245eeb
Serene Aryal <serene.aryal24@gmail.com>
2023-12-17 22:35:26 -0600

    group in tailwind

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -19,9 +19,9 @@ const Sidebar = () => {
 
 const SideBarIcon = ({ icon, text = "tooltip" }) => {
   return (
-    <div className="sidebar-icon">
+    <div className="sidebar-icon group">
       {icon}
-      <span className="sidebar-tooltip">{text}</span>
+      <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
     </div>
   );
 };
